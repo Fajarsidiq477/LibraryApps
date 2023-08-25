1ccf2c8e5c425f967100dac1b747f3c93bb4a85c
Fajar <fajars04@outlook.com>
2023-08-25 08:55:00 +0700

    fixing navbar active

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -13,7 +13,7 @@ class AdminController extends Controller
 {
 
     public function index() {
-
+        
         $books = Book::all();
         $transactions = Lend::class;
         $carbon = Carbon::class;
--- resources/views/partials/test/navbar-admin.blade.php
+++ resources/views/partials/test/navbar-admin.blade.php
@@ -3,24 +3,24 @@
         <div class="d-flex justify-content-between align-items-center" style="width: 100%">
             <ul class="navbar-nav flex-row">
                 <li class="nav-item">
-                    <a class="nav-link px-md-4 @if(Route::currentRouteName() === 'admin.index') active @endif" aria-current="page" href="{{ route('admin.index') }}">
+                    <a class="nav-link px-md-4 {{ request()->is('admin') ? 'active' : ''}}" aria-current="page" href="{{ route('admin.index') }}">
                         <i class="bi bi-house-fill"></i>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link px-md-4 @if(Route::currentRouteName() === 'books.index') active @endif" href="{{ route('books.index') }}">
+                    <a class="nav-link px-md-4 {{ request()->is('admin/books*') ? 'active' : ''}}" href="{{ route('books.index') }}">
                         <i class="bi bi-book-fill"></i>
                     </a>
                 </li>
                 @if (Auth::user()->role == 0)
                     <li class="nav-item">
-                        <a class="nav-link px-md-4 @if(Route::currentRouteName() === 'users.index') active @endif" href="{{ route('users.index') }}">
+                        <a class="nav-link px-md-4 {{ request()->is('admin/users*') ? 'active' : ''}}" href="{{ route('users.index') }}">
                             <i class="bi bi-person-fill"></i>
                         </a>
                     </li>
                 @endif
                 <li class="nav-item">
-                    <a class="nav-link px-md-4 @if(Route::currentRouteName() === 'lend-books.index') active @endif" href="{{ route('lend-books.index') }}">
+                    <a class="nav-link px-md-4 {{ request()->is('admin/lend-books*') ? 'active' : ''}}" href="{{ route('lend-books.index') }}">
                         <i class="bi bi-clock-fill"></i>
                     </a>
                 </li>
