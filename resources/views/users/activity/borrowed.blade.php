51ee04ff14f4a7d7e01512d54a89dbb63a422e40
Fajar <fajars04@outlook.com>
2023-08-27 15:08:01 +0700

    fixing user pages

--- resources/views/users/activity/borrowed.blade.php
+++ resources/views/users/activity/borrowed.blade.php
@@ -9,13 +9,13 @@
     <div class="container main-with-left-bar">
         <h3 class="text-center my-3">Buku yang Sedang Dipinjam</h3>
         
-        <p class="fw-bold">Jumlah buku: {{ $lend_data->count() }}</p>
+        <p class="fw-bold">Jumlah buku: {{ count($lends) }}</p>
 
-        @foreach($lend_data as $item)
+        @foreach($lends as $lend)
             <div class="row mt-3">
                 <div class="col-12 col-md-3 d-flex justify-content-center">
                     <img
-                        src="{{ asset('cover_images/' . $item->cover) }}"
+                        src="{{ asset('cover_images/' . $lend->book->cover) }}"
                         alt="book cover"
                         class=""
                         style="width:120px;"
@@ -23,31 +23,20 @@
                 </div>
                 <div class="col-12 col-sm-6 col-md-4 pl-4 d-flex mt-3">
                     <div>
-                        <h4 class="mb-2">{{ $item->title }}</h4>
-                        <p class="fw-bold mb-1">{{ $item->author }}</p>
-                        <p>{{ $item->publication_year }}</p>
+                        <h4 class="mb-2">{{ $lend->title }}</h4>
+                        <p class="fw-bold mb-1">{{ $lend->author }}</p>
+                        <p>{{ $lend->publication_year }}</p>
                     </div>
                 </div>
                 <div class="col-12 col-sm-6 col-md-5 d-flex mt-3">
                     <div>
                         <p>
                             <span class="fw-bold">Tanggal Pinjam:</span>
-                            <span>{{ $item->lend_date }}</span>
+                            <span>{{ $lend->lend_date }}</span>
                         </p>
                         <p>
                             <span class="fw-bold">Tanggal Kembali:</span>
-                            <span>{{ $item->return_date }}</span>
-                        </p>
-                        <p>
-                            <span class="fw-bold">Status:</span>
-                            @switch( $item->lend_status )
-                                @case(0)
-                                    <span class="badge bg-success">Dipinjam</span>
-                                    @break
-                                @case(3)
-                                    <span class="badge bg-warning">Telat</span>
-                                    @break
-                            @endswitch
+                            <span>{{ $lend->return_date }}</span>
                         </p>
                     </div>
                 </div>
