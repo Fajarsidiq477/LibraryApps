f8501ecc283f369c46101757ff9e923169e90400
Hadi501 <naxsulaxsanahadi@gmail.com>
2023-09-11 14:07:04 +0700

    gajadi update app.php dan database.php di config

--- config/app.php
+++ config/app.php
@@ -55,8 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://baitulhikmah.my.id'),
-    // 'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL'),
 
